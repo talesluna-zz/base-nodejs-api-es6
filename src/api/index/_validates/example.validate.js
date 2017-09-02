4bad8126df73e66b6025a517a6e1befc3c3178e2
Tales Luna <tales.ferreira.luna@gmail.com>
2017-09-02 18:28:00 -0300

    support to query filters, where, select, limit and offset

--- src/api/index/_validates/example.validate.js
+++ src/api/index/_validates/example.validate.js
@@ -18,21 +18,5 @@ export default (req, res, next) => {
             );
     });
 
-    Joi.object(
-        {
-            name: Joi.string()
-        }
-    ).validate(req.query, err => {
-        if (err)
-            return res.status(400).json(
-                {
-                    status : false,
-                    code   : 400,
-                    data   : '' + err.message,
-                    message: 'query_validate_error'
-                }
-            );
-    });
-
     next();
 }
