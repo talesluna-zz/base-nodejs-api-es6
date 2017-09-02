c3fa279b298a50d9820155dbbbee065e5cf2d256
Tales Luna <tales.ferreira.luna@gmail.com>
2017-09-02 18:32:11 -0300

    update environments

--- src/config/env/development.env.js
+++ src/config/env/development.env.js
@@ -1,31 +1,48 @@
 module.exports = {
     app: {
-        name: 'API Name',
-        version: '1.0.0'
+        name: 'API Name - DEV MODE',
+        version: '1.0.2'
     },
     server: {
+        secure: true,
         host: '127.0.0.1',
         port: 3000,
         cors: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         }
     },
     databases: {
         mongodb: {
-            host: '127.0.0.1',
+            host: 'localhost',
             port: 27017,
-            user: 'mongo-user',
-            pass: 'mongo-pass',
-            name: 'example'
+            user: 'username',
+            pass: 'password',
+            name: 'example?authSource=admin',
+            dialect: 'mongodb',
+            charset: 'utf8',
+            logging: true
         },
         mysql: {
-            host: '127.0.0.1',
+            host: 'localhost',
             port: 3306,
-            user: 'mysql-user',
-            pass: 'mysql-pass',
-            name: 'example'
+            user: 'admin',
+            pass: 'admin',
+            name: 'example',
+            dialect: 'mysql',
+            charset: 'utf8',
+            logging: true
+        },
+        postgres: {
+            host: 'localhost',
+            port: 5432,
+            user: 'admin',
+            pass: 'admin',
+            name: 'example',
+            dialect: 'postgres',
+            charset: 'utf8',
+            logging: true
         }
     }
 };
